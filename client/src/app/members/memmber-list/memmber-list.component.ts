1e3a510a07460cde97ce5b52ac42829d0667e605
EnesHarunANASOY <enesanasoy@gmail.com>
2024-08-04 21:24:40 +0300

    End of section 6

---
+++ client/src/app/members/memmber-list/memmber-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-memmber-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './memmber-list.component.html',
+  styleUrl: './memmber-list.component.css'
+})
+export class MemmberListComponent {
+
+}
